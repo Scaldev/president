d051c388b94e62bfff32a1e39429f22587504eac
Scald <scaldddd@gmail.com>
2023-08-29 17:32:05 +0200

    Routing works!

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 // Importer les modules nécessaires
 const express = require('express');
-const bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 
 // Créer une instance d'Express
 const app = express();
-
-// Middleware pour analyser les corps de requête JSON
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Route de base pour tester le serveur
@@ -15,12 +15,11 @@ app.get('/', (req, res) => {
 
 // Exemple de route pour ajouter un utilisateur à la base de données
 app.post('/utilisateurs', (req, res) => {
-    const { nom, email } = req.body;
-    console.log("hey", nom, email);
+    console.log("hey", req, res, req.body);
   
     // Simule une opération réussie, vous devriez ajouter ici la logique pour ajouter l'utilisateur à la base de données
     // En cas de succès, renvoyer une réponse avec un code de statut 201 (Créé)
-    res.status(201).json({ message: 'Utilisateur ajouté avec succès', nom, email });
+    res.status(201).json({ message: 'Utilisateur ajouté avec succès' });
   });
 
 // Port d'écoute du serveur
